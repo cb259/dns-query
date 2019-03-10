c8e96db68c06fd80dc6c05914ceaf4c1123059e4
cb <cburwell@gmail.com>
2019-03-10 18:47:36 -0400

    Initial commit from laptop

---
+++ dns-query.py
@@ -0,0 +1,62 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+import subprocess
+import sys
+
+#############################################################################################
+#
+# To-Do
+# - Validate host input (Required)
+# - Validate name server input (Default)
+# - Validate record type input (Accepted)
+# X Build web interface
+# X Fix issue with non-responsive resolver
+#############################################################################################
+
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST', 'GET'])
+def dnsQuery():
+    if request.method == 'GET':
+        # Render the main page
+        return render_template('dns-query.html')
+    elif request.method == 'POST':
+        # Arguments to send to method
+        hostname = str(request.form.get('hostname'))
+        name_server = str(request.form.get('name_server'))
+        record_type = str(request.form.get('record_type'))
+
+        # Build the results
+        result = resolver(hostname,name_server,record_type)
+
+        # Render the page with the results
+        return render_template('dns-query.html',result=result)
+
+def resolver(host, nameServer, recordType):
+    # Check if host is empty
+    if host == "":
+        return "No host provided!"
+
+    # Check if nameServer is empty
+    if nameServer == "":
+        # Set default resolver
+        nameServer = str("8.8.8.8")
+
+    # Check if recordType is empty
+    if recordType == "":
+        # Set default record type to ANY
+        recordType = str("ANY")
+
+    # Build command to run
+    command = "dig @" + nameServer + " " + host + " " + recordType
+
+    # Run command and store results
+    result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    # Return the command results
+    return result.stdout.read().decode(sys.stdout.encoding)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
