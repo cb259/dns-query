a40c03208dbde584d6fb5edec86f49289ea36c70
cb <cburwell@gmail.com>
2019-08-07 18:52:54 -0400

    Updates to use dnspython & code formatting on page render

--- dnsquery.py
+++ dnsquery.py
@@ -1,8 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
-import os
-import subprocess
-import sys
-
+import dns.resolver
 
 app = Flask(__name__)
 
@@ -14,39 +11,58 @@ def dnsQuery():
     elif request.method == 'POST':
         # Arguments to send to method
         hostname = str(request.form.get('hostname'))
-        name_server = str(request.form.get('name_server'))
+        #name_server = str(request.form.get('name_server'))
         record_type = str(request.form.get('record_type'))
 
         # Build the results
-        result = resolver(hostname,name_server,record_type)
+        result = resolver(hostname,record_type)
 
         # Render the page with the results
         return render_template('dns-query.html',result=result)
 
-def resolver(host, nameServer, recordType):
+def resolver(host, recordType):
     # Check if host is empty
     if host == "":
         return "No host provided!"
 
-    # Check if nameServer is empty
-    if nameServer == "":
-        # Set default resolver
-        nameServer = str("8.8.8.8")
-
-    # Check if recordType is empty
-    if recordType == "":
-        # Set default record type to ANY
-        recordType = str("ANY")
-
-    # Build command to run
-    command = "dig @" + nameServer + " " + host + " " + recordType
+    # Check recordType value
+    if recordType == "A":
+        recordType = str("A")
+    elif recordType == "ANY":
+        recordType = str("")
+    elif recordType == "CNAME":
+        recordType = str("CNAME")
+    elif recordType == "MX":
+        recordType = str("MX")
+    elif recordType == "NS":
+        recordType = str("NS")
+    elif recordType == "SOA":
+        recordType = str("SOA")
+    elif recordType == "SRV":
+        recordType = str("SRV")
+    elif recordType == "TXT":
+        recordType = str("TXT")
+    else:
+        recordType = str("")
 
     # Run command and store results
-    result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if recordType == "":
+        try:
+            query_result = dns.resolver.query(host)
+        except dns.resolver.NXDOMAIN:
+            return "ERROR: [NXDOMAIN] Unable to resolve domain as it does not exist."
+        except:
+            return "ERROR: [UNKNOWN] Unable to resolve the domain."
+    else:
+        try:
+            query_result = dns.resolver.query(host, recordType)
+        except dns.resolver.NXDOMAIN:
+            return "ERROR: [NXDOMAIN] Unable to resolve domain/record combination as it does not exist."
+        except:
+            return "ERROR: [UNKNOWN] Unable to resolve the domain/record combination."
 
     # Return the command results
-    return result.stdout.read().decode(sys.stdout.encoding)
-
+    return query_result.response
 
 if __name__ == '__main__':
-    app.run(debug = True)# Docker on Debian - Media Server
+    app.run(debug = True)
